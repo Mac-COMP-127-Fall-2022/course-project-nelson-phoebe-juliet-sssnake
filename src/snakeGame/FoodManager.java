9aeb6f3bacd6b77a8ec60992a36dadd1fa8f2a16
jcraymer <jcramer@macalester.edu>
2022-12-11 19:46:08 -0600

    Coding style for Food Manager class

--- src/snakeGame/FoodManager.java
+++ src/snakeGame/FoodManager.java
@@ -4,44 +4,58 @@ import java.util.Random;
 import edu.macalester.graphics.*;
 import java.util.ArrayList;
 
+/**
+ * This class keeps track of creating the food for the snake and
+ * destroying it when collsion has occured between itself and the head
+ * of the snake.
+ */
 public class FoodManager {
+    // instance variables
     private Random r = new Random();
     private CanvasWindow canvas;
     private Food food;
     private ArrayList<Point> gridPointList;
     private Point foodPos;
-
     private ImageManager imageManager;
-
     private String foodType;
 
-
+    /**
+    * Constructor for the FoodManager class
+    *
+    **/
     public FoodManager(CanvasWindow canvas, ArrayList<Point> gridPointList, String level, ImageManager imageManager){
         this.canvas = canvas;
-
         this.imageManager = imageManager;
-
         this.gridPointList = gridPointList;
-
         foodPos = new Point(0,0);
-
         food = new Food(foodPos.getX(), foodPos.getY());
     }
 
-    
-
+    /**
+    * Returns the type of food.
+    */
     public String getFoodType() {
         return foodType;
     }
 
+    /**
+    * Returns the upper left x-position of the food.
+    */
     public double getFoodX() {
         return foodPos.getX();
     }
 
+    /**
+    * Returns the upper left y-position of the food.
+    */
     public double getFoodY() {
         return foodPos.getY();
     }
 
+    /**
+    * Generates a food randomly and adds its corresponding
+    * image to the canvas.
+    */
     public void makeFood(ArrayList<SnakePiece> snake, SnakeHead head) { 
         randomlySetFoodType();
         randomlySetFoodPos(food,snake,head);
@@ -49,6 +63,11 @@ public class FoodManager {
         canvas.add(food.getShape());
     }
 
+    /**
+    * Randomly selects a type of food, depending on the probabilty
+    * for its type to be selected (30% of the time its a bug, 70%
+    * of the time it is a fruit).
+    */
     private void randomlySetFoodType(){
         int randomInt = r.nextInt(100);
         if(randomInt<=30){
@@ -58,6 +77,10 @@ public class FoodManager {
         }
     }
 
+    /**
+    * Randomly sets the position of the food within the canvas and
+    * ensures it is not set on the snake. 
+    */
     private void randomlySetFoodPos(Food food,ArrayList<SnakePiece> snake, SnakeHead head) {
         ArrayList<Point> foodPointList = new ArrayList<Point>();
         for(Point p: gridPointList){
